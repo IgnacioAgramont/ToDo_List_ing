a18b75234696a1d1c0a53dea96c897aa376a7300
Ignacio Agramont <ignacio.agramont.11@gmail.com>
2023-04-27 17:40:16 -0400

    test1

--- src/main/java/bo/edu/ucb/todo/api/AuthApi.java
+++ src/main/java/bo/edu/ucb/todo/api/AuthApi.java
@@ -2,31 +2,30 @@ package bo.edu.ucb.todo.api;
 
 import bo.edu.ucb.todo.bl.AuthBl;
 import bo.edu.ucb.todo.dto.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 class AuthApi {
 
     @PostMapping("/api/v1/auth/login")
-    public ResponseDto<TokenDto> login(@RequestBody LoginDto login) {
+    public ResponseEntity<ResponseDto<TokenDto>> login(@RequestBody LoginDto login) {
         ResponseDto<TokenDto> response = new ResponseDto<>();
         AuthBl authBl = new AuthBl();
         TokenDto tokenDto = authBl.login(login);
+        int statusCode = 200;
         if (tokenDto == null) {
             response.setCode("0001");
             response.setResponse(null);
             response.setErrorMessage("Invalid credentials");
-            return response;
+            statusCode = 401;
         } else {
             response.setCode("0000");
             response.setResponse(tokenDto);
-            return response;
         }
+        return ResponseEntity.status(statusCode).body(response);
     }
 }
--- src/main/java/bo/edu/ucb/todo/api/TodoApi.java
+++ src/main/java/bo/edu/ucb/todo/api/TodoApi.java
@@ -2,7 +2,6 @@ package bo.edu.ucb.todo.api;
 import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
